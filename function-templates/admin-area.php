b787c51bf0d7a691d67902da9490d0cb23bc02ed
Wes Mehaffy <wesonweb@gmail.com>
2021-06-30 09:58:34 +0100

    comments out remove admin items that causes fatal error

--- function-templates/admin-area.php
+++ function-templates/admin-area.php
@@ -33,18 +33,18 @@ function disable_dashboard_widgets() {
 add_action('wp_dashboard_setup', 'disable_dashboard_widgets');
 
 // remove items from the admin area
-function remove_admin_menu_items() {
-    $remove_menu_items = array(__ ()
-    );
-    global $menu;
-    end ($menu);
-    while (prev($menu)){
-    $item = explode(' ',$menu[key($menu)][0]);
-    if(in_array($item[0] != NULL?$item[0]:"" , $remove_menu_items)){
-    unset($menu[key($menu)]);}
-    }
-}
-add_action('admin_menu', 'remove_admin_menu_items');
+// function remove_admin_menu_items() {
+//     $remove_menu_items = array(__ ()
+//     );
+//     global $menu;
+//     end ($menu);
+//     while (prev($menu)){
+//     $item = explode(' ',$menu[key($menu)][0]);
+//     if(in_array($item[0] != NULL?$item[0]:"" , $remove_menu_items)){
+//     unset($menu[key($menu)]);}
+//     }
+// }
+// add_action('admin_menu', 'remove_admin_menu_items');
 
 //remove screen options from admin area
 add_filter( 'screen_options_show_screen', '__return_false' );
